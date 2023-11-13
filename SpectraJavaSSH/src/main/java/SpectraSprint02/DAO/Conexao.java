22eb2aa01418063ebd83ada6fe040cf60dc03720
Murilo Almeida <murilo.leite@sptech.school>
2023-11-13 09:11:02 -0300

    @MuriloAlmeid arrumando conexao Slack

--- SpectraJavaSSH/src/main/java/SpectraSprint02/DAO/Conexao.java
+++ SpectraJavaSSH/src/main/java/SpectraSprint02/DAO/Conexao.java
@@ -7,24 +7,7 @@ public class Conexao {
     JdbcTemplate conexaoDoBancoMySQL;
     JdbcTemplate conexaoDoBancoSQLServer;
 
-    public Conexao() {/*
-        BasicDataSource dataSource01 = new BasicDataSource();
-        BasicDataSource dataSource02 = new BasicDataSource();
-
-        dataSource01.setDriverClassName("com.mysql.jdbc.Driver");
-
-        dataSource01.setUrl("jdbc:mysql://localhost:3306/Spectra");
-        dataSource01.setUsername("Spectra");
-        dataSource01.setPassword("Spectra123");
-
-        dataSource02.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSource02.setUrl("jdbc:sqlserver://52.45.176.88:1433;database=Spectra");
-        dataSource02.setUsername("Spectra");
-        dataSource02.setPassword("Spectra123");
-
-        conexaoDoBanco = new JdbcTemplate(dataSource01);
-        conexaoDoBanco = new JdbcTemplate(dataSource02);*/
-
+    public Conexao() {
         // Configuração para MySQL
         BasicDataSource dataSourceMySQL = new BasicDataSource();
         dataSourceMySQL.setDriverClassName("com.mysql.cj.jdbc.Driver");
@@ -36,7 +19,7 @@ public class Conexao {
         // Configuração para SQL Server
         BasicDataSource dataSourceSQLServer = new BasicDataSource();
         dataSourceSQLServer.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSourceSQLServer.setUrl("jdbc:sqlserver://52.45.176.88:1433;databaseName=Spectra;encrypt=true;trustServerCertificate=true;");
+        dataSourceSQLServer.setUrl("jdbc:sqlserver://52.45.176.88;databaseName=Spectra;encrypt=true;trustServerCertificate=true;");
         dataSourceSQLServer.setUsername("Spectra");
         dataSourceSQLServer.setPassword("Spectra123");
         conexaoDoBancoSQLServer = new JdbcTemplate(dataSourceSQLServer);
