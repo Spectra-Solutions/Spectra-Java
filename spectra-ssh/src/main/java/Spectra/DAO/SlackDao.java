4615be894fdd69d068a467fb7729f39938410832
Murilo Almeida <murilo.leite@sptech.school>
2023-11-20 18:22:11 -0300

    @MuriloAlmeid

---
+++ spectra-ssh/src/main/java/Spectra/DAO/SlackDao.java
@@ -0,0 +1,35 @@
+package Spectra.DAO;
+
+import Spectra.Connection.ConexaoMysQl;
+import Spectra.DTO.Empresa;
+import Spectra.DTO.Maquina;
+import Spectra.Slack.Slack;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlackDao {
+    Slack slack = new Slack();
+    List<Empresa> empresas;
+    List<String> empresasCadastradas;
+    ConexaoMysQl conexaoMysQl = new ConexaoMysQl();
+    protected JdbcTemplate conMySQl = conexaoMysQl.getConexaoMySQl();
+
+    public SlackDao() {
+        this.empresas = new ArrayList<>();
+    }
+
+    public void getNomeEmpresa(String hostName){
+        String sql = "SELECT nomeEmpresa FROM EMPRESA";
+
+        String nomeEmpresa = conMySQl.queryForObject(sql, String.class, hostName);
+
+        for (Empresa e: empresas) {
+            if (!nomeEmpresa.equalsIgnoreCase(e.getNomeEmpresa())){
+                empresasCadastradas.add(nomeEmpresa);
+            }
+        }
+
+    }
+}
---
+++ spectra-ssh/src/main/java/Spectra/VIEW/SistemaSpectra.java
@@ -0,0 +1,15 @@
+package Spectra.VIEW;
+
+import Spectra.Slack.Slack;
+import com.slack.api.model.admin.AppConfig;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class SistemaSpectra {
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        IniciarSistema iniciarSistema = new IniciarSistema();
+//
+//        iniciarSistema.validarLogin();
+    }
+}
